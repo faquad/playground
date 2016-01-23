e97230c7033e61eef6d2cfdf25ff027dfd9c5902
Khiem Nguyen <khiemu@googlemail.com>
2016-01-23 23:01:34 +0100

    db

--- offsite/db/src/main/java/de/khiem/offsite/db/Database.java
+++ offsite/db/src/main/java/de/khiem/offsite/db/Database.java
@@ -3,9 +3,15 @@ package de.khiem.offsite.db;
 
 
 public interface Database {
-     <T> QueryResult<T>  findEntityById(Long id);
+     TransactionContext transContext();
+    
+     <T extends Entity> QueryResult findById (Long id, Class<T> cls);
+     
      <T> QueryResult<T> query(DbQuery query); 
      
      void save(TransactionContext ctx); 
+     
      void delete(TransactionContext  ctx);
+     
+     <T extends Dao> T daoInstance(Class<T> cls);
 }
